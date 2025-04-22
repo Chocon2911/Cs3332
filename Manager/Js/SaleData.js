1b7f37bbe4d9212c4cdfba32fc58174756f30eb6
Chocon2911 <131384762+Chocon2911@users.noreply.github.com>
2025-04-22 17:28:35 +0700

    XHuy - Day1 (2)
    SaleData
    + Can display chart
    + Can display checkbox list

--- Manager/Js/SaleData.js
+++ Manager/Js/SaleData.js
@@ -1,34 +1,3 @@
-const productDbs = [
-    new ProductDb(0, "Cappuccino", 100),
-    new ProductDb(1, "Latte", 50),
-    new ProductDb(2, "Espresso", 20),
-    new ProductDb(3, "Cappuccino", 100),
-    new ProductDb(4, "Latte", 50),
-    new ProductDb(5, "Espresso", 20),
-    new ProductDb(6, "Cappuccino", 100),
-    new ProductDb(7, "Latte", 50),
-    new ProductDb(8, "Espresso", 20),
-    new ProductDb(9, "Cappuccino", 100),
-    new ProductDb(10, "Latte", 50),
-    new ProductDb(11, "Espresso", 20),
-    new ProductDb(12, "Cappuccino", 100),
-    new ProductDb(13, "Latte", 50),
-    new ProductDb(14, "Espresso", 20),
-    new ProductDb(15, "Cappuccino", 100),
-    new ProductDb(16, "Latte", 50),
-    new ProductDb(17, "Espresso", 20),
-    new ProductDb(9, "Cappuccino", 100),
-    new ProductDb(10, "Latte", 50),
-    new ProductDb(11, "Espresso", 20),
-    new ProductDb(12, "Cappuccino", 100),
-    new ProductDb(13, "Latte", 50),
-    new ProductDb(14, "Espresso", 20),
-    new ProductDb(15, "Cappuccino", 100),
-    new ProductDb(16, "Latte", 50),
-    new ProductDb(17, "Espresso", 20)
-];
-
-
 //============================================Obj=============================================
 class ProductDb
 {
@@ -95,6 +64,21 @@ class DataSet
 }
 
 //==========================================Support===========================================
+function getDataSet(name, revenues)
+{
+    const rgb = randomRgb();
+    const dataset = new DataSet(
+        name,
+        revenues,
+        randomColor(rgb),
+        randomBorderColor(rgb),
+        true,
+        0
+    );
+
+    return dataset;
+}
+
 function randomRgb() 
 {
     return [Math.floor(Math.random() * 200), Math.floor(Math.random() * 200), Math.floor(Math.random() * 200)];
@@ -114,7 +98,7 @@ function getDateTime(ms)
 {
     const date = new Date(ms);
     const year = date.getFullYear();
-    const month = date.getMonth() + 1;
+    const month = date.getMonth();
     const day = date.getDate();
     const hour = date.getHours();
     const minute = date.getMinutes();
@@ -127,53 +111,87 @@ function getProducts(productDbs)
 {
     const products = [];
 
+    // go through all children in productDbs (input data)
     for (let i = 0; i < productDbs.length; i++)
     {
-        for (let j = 0; j < productDbs[i].productDatas.length; j++)
+        // first product
+        if (products.length == 0)
         {
-            if (products.find(p => p.productName == productDbs[i].productName) == null) 
+            products.push(new Product(productDbs[i].productName, []));
+            productData = new ProductData(getDateTime(productDbs[i].ms), productDbs[i].price);
+            products[0].productDatas.push(productData);
+        }
+
+        // After first product
+        else
+        {
+            // go through all product in products
+            for (let j = 0; j < products.length; j++)
             {
-                products.push(new Product(productDbs[i].productName, []));
+                console.log(products[j].productName);
+                // curr product has same name as products[i]
+                if (products[j].productName.localeCompare(productDbs[i].productName) === 0)
+                {
+                    productData = new ProductData(getDateTime(productDbs[i].ms), productDbs[i].price);
+                    products[j].productDatas.push(productData);
+                    break;
+                }
+
+                // current product has different name and last check
+                else if (j == products.length - 1) 
+                {
+                    products.push(new Product(productDbs[i].productName, []));
+                    productData = new ProductData(getDateTime(productDbs[i].ms), productDbs[i].price);
+                    products[products.length - 1].productDatas.push(productData);
+                    break;
+                }
+
+                // current product has different name
+                else continue;
             }
-            products.find(p => p.productName == productDbs[i].productName).productDatas.push(new ProductData(getDateTime(productDbs[i].ms), productDbs[i].price));
         }
     }
+
+    return products;
 }
 
 //========================================Create Chart========================================
-function displayChart()
-{
-
-}
-
-function createChartInADay(date, products, xLabelName, yLabelName)
+function createChartInADay(productDbs, xLabelName, yLabelName)
 {
     const labelCount = 24;
     const xLabels = [];
 
-    // x label from 00h to 23h
+    // x label from 01h to 24h
     for (let i = 0; i < labelCount; i++) 
     {
-        const hour = i;
-        const timeString = `${String(hour).padStart(2, '0')}`;
+        const hour = i + 1;
+        const timeString = `${String(hour).padStart(2, '0')}:00`;
         xLabels.push(timeString);
     }
 
-    // revenue datas from 00h to 23h
-    const products = getProducts(productDb);
-
-    const revenues = [];
-    for (let i = 0; i < productDbs.length; i++)
+    // revenue datas from 01h to 24h
+    const products = getProducts(productDbs);
+    const revenueDatas = [];
+    for (let i = 0; i < products.length; i++)
     {
+        const revenues = Array(labelCount).fill(0);
+        for (let j = 0; j < products[i].productDatas.length; j++)
+        {
+            const prodData = products[i].productDatas[j];
+            const hour = prodData.time.hour;
+            revenues[hour] += prodData.revenue;
+        }
         
+        const dataset = getDataSet(products[i].productName, revenues);
+        revenueDatas.push(dataset);
     }
 
-    createChart(date, date, products, xLabelName, yLabelName, xLabels);
+    createChart(xLabelName, yLabelName, xLabels, revenueDatas);
 }
 
-function createChartInAMonth(date, products, xLabelName, yLabelName)
+function createChartInAMonth(date, productDbs, xLabelName, yLabelName)
 {
-    labelCount = 0;
+    let labelCount = 0;
 
     if (date.day == 1 || date.day == 3 || date.day == 5 || date.day == 7 || date.day == 8 || date.day == 10 || date.day == 12)
     {
@@ -200,10 +218,27 @@ function createChartInAMonth(date, products, xLabelName, yLabelName)
         xLabels.push(timeString);
     }
 
-    createChart(date, date, products, xLabelName, yLabelName, xLabels);
+    // revenue datas from 01 to last day of month
+    const products = getProducts(productDbs);
+    const revenueDatas = [];
+    for (let i = 0; i < products.length; i++)
+    {
+        const revenues = Array(labelCount).fill(0);
+        for (let j = 0; j < products[i].productDatas.length; j++)
+        {
+            const prodData = products[i].productDatas[j];
+            const day = prodData.time.day;
+            revenues[day - 1] += prodData.revenue;
+        }
+        
+        const dataset = getDataSet(products[i].productName, revenues);
+        revenueDatas.push(dataset);
+    }
+
+    createChart(xLabelName, yLabelName, xLabels, revenueDatas);
 }
 
-function createChartInAYear(date, products, xLabelName, yLabelName)
+function createChartInAYear(productDbs, xLabelName, yLabelName)
 {
     const labelCount = 12;
     const xLabels = [];
@@ -216,43 +251,28 @@ function createChartInAYear(date, products, xLabelName, yLabelName)
         xLabels.push(timeString);
     }
 
-    createChart(date, date, products, xLabelName, yLabelName, xLabels);
-}
-
-function createChart(fromDate, toDate, products, xLabelName, yLabelName, xLabels) 
-{
-    const labelCount = 25;
-    const fromSec = fromDate.getTotalSecond();
-    const toSec = toDate.getTotalSecond();
-
+    // revenue datas from 01 to 12
+    const products = getProducts(productDbs);
     const revenueDatas = [];
-
-    // list data of drink for chart to draw
-    for (let i = 0; i < products.length; i++) 
+    for (let i = 0; i < products.length; i++)
     {
         const revenues = Array(labelCount).fill(0);
-        for (let j = 0; j < products[i].productDatas.length; j++) 
+        for (let j = 0; j < products[i].productDatas.length; j++)
         {
             const prodData = products[i].productDatas[j];
-            const t = prodData.time.getTotalSecond();
-            const percent = (t - fromSec) / (toSec - fromSec);
-            const idx = Math.floor(percent * labelCount);
-            if (idx >= 0 && idx < labelCount) revenues[idx] += prodData.revenue;
+            const month = prodData.time.month;
+            revenues[month - 1] += prodData.revenue;
         }
-
-        const rgb = randomRgb();
-        const dataSet = new DataSet(
-            products[i].productName,
-            revenues,
-            randomColor(rgb),
-            randomBorderColor(rgb),
-            true,
-            0);
-
-        revenueDatas.push(dataSet);
+        
+        const dataset = getDataSet(products[i].productName, revenues);
+        revenueDatas.push(dataset);
     }
 
-    // Draw chart from datas above
+    createChart(products, xLabelName, yLabelName, xLabels);
+}
+
+function createChart(xLabelName, yLabelName, xLabels, revenueDatas) 
+{
     const ctx = document.getElementById('SaleChart').getContext('2d');
     new Chart(ctx, {
         type: 'line',
@@ -298,19 +318,58 @@ function createChart(fromDate, toDate, products, xLabelName, yLabelName, xLabels
 }
 
 //======================================Display Products======================================
-function displayProducts(productNames)
+function createProductCheckBoxs(productNames)
 {
     for (let i = 0; i < productNames.length; i++)
     {
         let product = document.createElement("div");
         product.classList.add("ProductItem");
-        product.innerHTML = `<input type="checkbox">${productNames[i]}</input>`;
-        productContainer.appendChild(product);
+        product.innerHTML = `<input type="checkbox" value="${productNames[i]}">${productNames[i]}</input>`;
+        document.getElementById("Products").appendChild(product);
     }
+}
+
+//==========================================Display===========================================
+const productNames = ["Cappuccino", "Latte", "Espresso"];
+const productDbs = [
+    new ProductDb(10000000, "Cappuccino", 100),
+    new ProductDb(1, "Latte", 50),
+    new ProductDb(2, "Espresso", 20),
+    new ProductDb(50000000, "Cappuccino", 100),
+    new ProductDb(4, "Latte", 50),
+    new ProductDb(5, "Espresso", 20),
+    new ProductDb(6, "Cappuccino", 100),
+    new ProductDb(7, "Latte", 50),
+    new ProductDb(8, "Espresso", 20),
+    new ProductDb(9, "Cappuccino", 100),
+    new ProductDb(10, "Latte", 50),
+    new ProductDb(11, "Espresso", 20),
+    new ProductDb(12, "Cappuccino", 100),
+    new ProductDb(20000000, "Latte", 50),
+    new ProductDb(14, "Espresso", 20),
+    new ProductDb(15, "Cappuccino", 100),
+    new ProductDb(16, "Latte", 50),
+    new ProductDb(17, "Espresso", 20),
+    new ProductDb(9, "Cappuccino", 100),
+    new ProductDb(10, "Latte", 50),
+    new ProductDb(11, "Espresso", 20),
+    new ProductDb(12, "Cappuccino", 100),
+    new ProductDb(13, "Latte", 50),
+    new ProductDb(14, "Espresso", 20),
+    new ProductDb(15, "Cappuccino", 100),
+    new ProductDb(16, "Latte", 50),
+    new ProductDb(17, "Espresso", 20)
+];
+
+function displayChart()
+{
+    createChartInADay(productDbs, "Time (hour)", "Revenue ($)");
 }
 
 function displayProducts()
 {
-
+    createProductCheckBoxs(productNames);
 }
 
+displayChart();
+displayProducts();
